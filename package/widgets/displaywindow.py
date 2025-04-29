c5e6c78cb3511f946bf7e77019709a85cefefc2f
famnkunf <cuong26022003@gmail.com>
2025-04-29 10:10:01 +0700

    update displaywindow

--- package/widgets/displaywindow.py
+++ package/widgets/displaywindow.py
@@ -1,12 +1,14 @@
 from PyQt5 import QtWidgets, QtGui, QtCore
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt5 import NavigationToolbar2QT as NavigationToolbar
+import matplotlib as mpl
 from matplotlib import pyplot as plt
 from astropy.visualization import ImageNormalize, LinearStretch
 
 from ..ui import displaywindow_ui
 from .headerwindow import HeaderWindow
 
+mpl.use('Qt5Agg')
 class DisplayWindow(QtWidgets.QWidget):
     def __init__(self, image, title, header_data=None, main_window=None):
         super().__init__()
@@ -17,46 +19,32 @@ class DisplayWindow(QtWidgets.QWidget):
         self.header_data = header_data
         self.setWindowTitle(self.title)
         self.setGeometry(0, 0, 800, 600)
-        self.canvas = FigureCanvas(Figure(figsize=(self.image.shape[1] / 100, self.image.shape[0] / 100), dpi=100))
-        self.canvas.mpl_connect("motion_notify_event", self.on_mouse_move)
-        self.canvas.mpl_connect('scroll_event', self.on_scroll)
-        self.canvas.mpl_connect('button_press_event', self.start_pan)
-        self.canvas.mpl_connect('button_release_event', self.stop_pan)
-        self.canvas.mpl_connect('motion_notify_event', self.on_pan)
+        self.figure, self.ax = plt.subplots()
+        self.ax.axis('off')
+        self.canvas = FigureCanvas(self.figure)
+        self.toolbar = NavigationToolbar(self.canvas, self)
+        self.toolbar.hide()
         self.norm = ImageNormalize(self.image, stretch=LinearStretch())
         self.norm.vmin = self.image.mean() - 0 * self.image.std()
         self.norm.vmax = self.image.mean() + 0.5 * self.image.std()
         self.ui.verticalLayout_2.addWidget(self.canvas)
         self.ui.OpenHeader.clicked.connect(self.open_header)
+        self.ui.pan_and_zoom.clicked.connect(self.pan_and_zoom)
+        self.ui.home.clicked.connect(self.toolbar.home)
         self.plot_image()
         self.setFocusPolicy(QtCore.Qt.StrongFocus)
         self.main_window = main_window
         
-    def on_mouse_move(self, event):
-        if self.main_window.aperture_window:
-            pass
-    
-    def on_scroll(self, event):
-        pass
-    
-    def start_pan(self, event):
-        pass
-
-    def stop_pan(self, event):
-        pass
-    
-    def on_pan(self, event):
-        pass
-    
+    def pan_and_zoom(self):
+        self.toolbar.pan()
+        
     def focusInEvent(self, event: QtGui.QFocusEvent):
         if self.main_window.histogram_window:
             self.main_window.histogram_window.set_data(self)
         return super().focusInEvent(event)
     
     def plot_image(self):
-        ax = self.canvas.figure.add_subplot(111)
-        ax.imshow(self.image, cmap='gray', origin='lower', norm=self.norm)
-        ax.axis('off')
+        self.ax.imshow(self.image, cmap='gray', norm=self.norm, interpolation='bicubic', interpolation_stage='data')
         self.canvas.draw()
         
     def open_header(self):
