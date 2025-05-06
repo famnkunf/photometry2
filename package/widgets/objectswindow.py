004c7134246822509ca3c97b8e92f7ee6f5a88a1
famnkunf <cuong26022003@gmail.com>
2025-05-06 13:13:54 +0700

    update objects window

--- package/widgets/objectswindow.py
+++ package/widgets/objectswindow.py
@@ -1,5 +1,6 @@
 from PyQt5 import QtWidgets, QtCore, QtGui
 from vispy import scene, util
+import numpy as np
 
 from ..ui.objectswindow_ui import Ui_ObjectsWindow
 
@@ -12,8 +13,11 @@ class ObjectsWindow(QtWidgets.QWidget):
         self.setWindowTitle("Objects")
         self.setGeometry(1000, 200, 500, 400)
         self.objects = []
+        self.selected_object = None
+        self.selected_object_marker = None
         self.ui.export_button.clicked.connect(self.export_objects)
         self.ui.objects_table.cellChanged.connect(self.update_object)
+        self.ui.objects_table.itemSelectionChanged.connect(self.update_selected_object)
         
     def add_new_object(self, display_window, x, y, major_axis, minor_axis, angle, gap, background, intensity):
         aperture = scene.Node(parent=display_window.view.scene)
@@ -31,36 +35,56 @@ class ObjectsWindow(QtWidgets.QWidget):
             'id': len(self.objects),
             'x': x,
             'y': y,
+            'intensity': intensity,
+            'name': '',
+            'notes': '',
             'major_axis': major_axis,
             'minor_axis': minor_axis,
             'angle': angle,
             'gap': gap,
             'background': background,
             'aperture': aperture,
-            'intensity': intensity,
-            'aperture': aperture,
-            'name': '',
-            'notes': '',
+            'display_window': display_window,
+            'file_name': display_window.title.split("/")[-1],
         }
         self.objects.append(new_object)
         self.update_table()
+    
+    def update_selected_object(self):
+        if self.selected_object_marker is not None:
+            self.selected_object_marker.parent = None
+        else:
+            self.selected_object_marker = scene.Markers()
+            self.selected_object_marker.transform = scene.STTransform(translate=(0, 0, -1), scale=(1, 1, 1))     
+        if len(self.ui.objects_table.selectedItems()) > 0:
+            row = self.ui.objects_table.selectedItems()[0].row()
+            if row >= len(self.objects):
+                return
+            self.selected_object = self.objects[row]
+            x, y = self.selected_object['x'], self.selected_object['y']
+            display_window = self.selected_object['display_window']
+            self.selected_object_marker.parent = display_window.view.scene
+            self.selected_object_marker.set_data(pos=np.array([[x, y]]), face_color='red', size=10)
+            self.selected_object['display_window'].raise_()
+        else:
+            self.selected_object = None
+            self.selected_object_marker.parent = None
         
     def update_object(self, row, column):
         if row >= len(self.objects):
             return
         if column == 0:
             return
-        if column == 2:
+        if column == 1:
             self.objects[row]['x'] = float(self.ui.objects_table.item(row, column).text())
-        elif column == 3:
+        elif column == 2:
             self.objects[row]['y'] = float(self.ui.objects_table.item(row, column).text())
-        elif column == 9:
+        elif column == 3:
             self.objects[row]['intensity'] = float(self.ui.objects_table.item(row, column).text())
-        elif column == 10:
+        elif column == 4:
             self.objects[row]['name'] = self.ui.objects_table.item(row, column).text()
-        elif column == 11:
+        elif column == 5:
             self.objects[row]['notes'] = self.ui.objects_table.item(row, column).text()
-        print(row, column)
     
     def update_table(self):
         if len(self.objects) == 0:
@@ -73,7 +97,7 @@ class ObjectsWindow(QtWidgets.QWidget):
             for j, k in enumerate(obj.keys()):
                 item = QtWidgets.QTableWidgetItem(str(obj[k]))
                 self.ui.objects_table.setItem(i, j, item)
-        for i in range(3, 9):
+        for i in range(6, 14):
             self.ui.objects_table.hideColumn(i)
         self.ui.objects_table.resizeColumnsToContents()
         self.ui.objects_table.resizeRowsToContents()
